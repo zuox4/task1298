e86914bef166419a387c6328e4681e71a1d1555b
zuox4 <kiryxa97g@gmail.com>
2024-11-08 10:46:44 +0300

    Update AllTasksList.jsx

--- src/Root/TaskShop/AllTasksList.jsx
+++ src/Root/TaskShop/AllTasksList.jsx
@@ -4,9 +4,7 @@ import Task from "./Task/Task.jsx";
 const AllTasksList = ({ filterTask, data }) => {
     return (
         <div
-            className={'all-task-list'}
-            style={{ marginBottom: '100px', alignItems: 'center', marginTop: filterTask ? '15px' : '70px', display: 'flex', flexDirection: 'column', gap: '25px', overflow: 'hidden' }}
-        >
+            className={'all-task-list'}>
             {data.map((task, index) => (
                 <div key={index}>
                     {/* Здесь вы можете отобразить свойства задачи, например: */}
